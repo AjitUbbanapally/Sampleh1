bccd32cd4a991d6439179f56ff44a7587bc46843
AjitUbbanapally <Ajitpurnahk@gmail.com>
2024-02-20 12:16:43 -0600

    Autolayout, outlets

--- Sampleh1/ViewController.swift
+++ Sampleh1/ViewController.swift
@@ -8,11 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var listTV: UITextView!
+    
+    
+    @IBOutlet weak var itemTF: UITextField!
+    
+    @IBOutlet weak var itemQuantityTF: UITextField!
+    
+    @IBOutlet weak var itemNumTF: UITextField!
+    
+    @IBOutlet weak var addBTN: UIButton!
+    
+    @IBOutlet weak var clearBTN: UIButton!
+    
+    
+    @IBOutlet weak var deleteBTN: UIButton!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func addToList(_ sender: Any) {
+    }
+    
+    
+    @IBAction func clear(_ sender: Any) {
+    }
+    
+    
+    @IBAction func deleteFromList(_ sender: Any) {
+    }
+    
     
 }
 
